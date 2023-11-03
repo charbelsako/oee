cdea971f68600b1d8bfcfbb362304ff0d40107d0
Charbel Sarkis <charbelsako@gmail.com>
2023-11-03 17:51:48 +0200

    Updates message text

--- app/helpers.php
+++ app/helpers.php
@@ -6,7 +6,7 @@ if (!function_exists("check_user_has_not_permission")) {
     function check_user_has_not_permission($permission='') {
 
         /*if(!auth()->check()){
-            return redirect('/home')->with('msgstatus', 'error')->with('messagetext','You are not login');
+            return redirect('/home')->with('msgstatus', 'error')->with('messagetext','You are not logged in');
         }
         if($permission == '' || auth()->user()->cannot($permission)) {*/
             throw new UnauthorizedException(403,"Access denied");
