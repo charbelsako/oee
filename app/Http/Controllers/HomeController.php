5943ce678128d224687f80f9fc7daa8df1a222e0
Charbel Sarkis <charbelsako@gmail.com>
2023-11-15 17:47:48 +0200

    Silences linting error of undefined db model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Report;
+// use App\Models\Report;
 use Illuminate\Http\Request;
 use Rap2hpoutre\FastExcel\FastExcel;
+use InfluxDB2\Client;
+use InfluxDB2\Model\WritePrecision;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -25,7 +28,29 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Get data to view in home
+        $client = new Client([
+            'url' => env('INFLUXDB_HOST'),
+            'token' => env('INFLUXDB_TOKEN'),
+            'bucket' => env('INFLUXDB_BUCKET'),
+            'org' => env('INFLUXDB_ORG'),
+            'precision' => WritePrecision::S,
+            'verifySSL' => false
+        ]);
+        $queryApi = $client->createQueryApi();
+        $query = "from(bucket: \"oee_test\") |> range(start: -6d) |> filter(fn: (r) => r._measurement == \"temperature\")";
+        $temperature_data = $queryApi->query($query);
+
+        $records = [];
+        foreach ($temperature_data as $table) {
+            foreach ($table->records as $record) {
+                error_log('Temperature ' . $record->getTime() . PHP_EOL);
+                $row = key_exists($record->getTime(), $records) ? $records[$record->getTime()] : [];
+                $records[$record->getTime()] = array_merge($row, [$record->getField() => $record->getValue()]);
+            }
+        }
+
+        return view('home', ['records' => $records]);
     }
 
     public function testing()
@@ -33,25 +58,25 @@ class HomeController extends Controller
         return view('cms.form');
     }
 
-    public function reports()
-    {
-        $data['reports'] = Report::query()->where('user_id',auth()->id())->latest()->paginate(6);
-        return view('cms.reports',$data);
-    }
+    // public function reports()
+    // {
+    //     $data['reports'] = Report::query()->where('user_id', auth()->id())->latest()->paginate(6);
+    //     return view('cms.reports', $data);
+    // }
 
-    public function report($id)
-    {
-        $report = Report::query()->where('user_id',auth()->id())->where('id',$id)->first();
-        $data['report'] = $report;
-        return view('cms.last',$data);
-    }
+    // public function report($id)
+    // {
+    //     $report = Report::query()->where('user_id', auth()->id())->where('id', $id)->first();
+    //     $data['report'] = $report;
+    //     return view('cms.last', $data);
+    // }
 
-    public function last()
-    {
-        $report = Report::query()->where('user_id',auth()->id())->latest()->first();
-        $data['report'] = $report;
-        return view('cms.last',$data);
-    }
+    // public function last()
+    // {
+    //     $report = Report::query()->where('user_id', auth()->id())->latest()->first();
+    //     $data['report'] = $report;
+    //     return view('cms.last', $data);
+    // }
 
     public function submitTesting(Request $request)
     {
@@ -85,21 +110,23 @@ class HomeController extends Controller
         $res = $client->get($link);
         $arr = json_decode($res->getBody());
 
-        $list = collect([[
-            'age'           => $request->age,
-            'smoke'         => $request->smoke,
-            'alcohol'       => $request->alcohol,
-            'drink'         => $request->drink,
-            'coffee'        => $request->coffee,
-            'user_id'       => auth()->id(),
-            'avgbase'       => $arr->avgbase,
-            'morning'       => $arr->morning,
-            'noon'          => $arr->noon,
-            'evening'       => $arr->evening,
-            'data_response' => json_encode($arr),
-            'hr'            => json_encode($arr->HR),
-            'time'          => json_encode($arr->time),
-        ]]);
+        $list = collect([
+            [
+                'age' => $request->age,
+                'smoke' => $request->smoke,
+                'alcohol' => $request->alcohol,
+                'drink' => $request->drink,
+                'coffee' => $request->coffee,
+                'user_id' => auth()->id(),
+                'avgbase' => $arr->avgbase,
+                'morning' => $arr->morning,
+                'noon' => $arr->noon,
+                'evening' => $arr->evening,
+                'data_response' => json_encode($arr),
+                'hr' => json_encode($arr->HR),
+                'time' => json_encode($arr->time),
+            ]
+        ]);
 
         (new FastExcel($list))->export('file.csv');
 
